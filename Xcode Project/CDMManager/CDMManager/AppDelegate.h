c372712a3127fd6e6d77b0bc727dff332489909f
William Gustafson <notmyrealemail@icloud.com>
2020-01-30 16:40:06 -0500

    Restructure of Xcode Project Folder Hierarchy  + Add 1.0 Release DMG

---
+++ Xcode Project/CDMManager/CDMManager/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  CDMManager
+//
+//  Created by William Gustafson on 1/6/20.
+//
+// Credit for the implementation goes mostly to Phil Dennis-Jordan (https://philjordan.eu)
+// who provided critical guidance and code examples via:
+// https://stackoverflow.com/questions/59594123/enabling-closed-display-mode-w-o-meeting-apples-requirements
+
+#import <Cocoa/Cocoa.h>
+#import <IOKit/pwr_mgt/IOPMLib.h>
+
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@end
+
---
+++ Xcode Project/CDMManager/CDMManager/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CDMManager
+//
+//  Created by William Gustafson on 1/6/20.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool
+    {
+    }
+    return NSApplicationMain(argc, argv);
+}
